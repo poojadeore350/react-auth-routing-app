022436eb07ead0e79c6473b5f8e3f716e5ce874e
poojadeore350 <poojadeore350@gmail.com>
2026-03-29 17:32:50 +0530

    Updated login with API and routing flow

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Navigate } from "react-router-dom";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ProtectedRoute from './Components/ProtectedRoute';
 import  Login  from './Pages/Login';
 import  Dashboard  from './Pages/Dashboard';
 import  Profile  from './Pages/Profile';
 import  NotFound  from './Pages/NotFound';
-import ProtectedRoute from './Components/ProtectedRoute';
+
 
 function App() {
   return (
   <BrowserRouter>
   <Routes>
+    <Route path="/" element={<Navigate to="/login" />} />
     <Route path="/login" element={<Login />} />
     <Route path="/dashboard" element={
       <ProtectedRoute>
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-    <div className="notfound-container">
-        <div className="notfound-card">
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
-        <p>The page you are looking for does not exist.</p>
+  const navigate = useNavigate();
 
-        <a href="/dashboard" className="home-btn">
-            Go Back Home
-        </a>
+  return (
+    <div className="notfound-container">
+      <div className="notfound-card">
+        
+        <h1 className="notfound-code">404</h1>
+
+        <h2 className="notfound-title">Page Not Found</h2>
+
+        <p className="notfound-text">
+          Sorry, the page you’re looking for doesn’t exist or has been moved.
+        </p>
+
+        <div className="notfound-buttons">
+          <button onClick={() => navigate("/login")}>
+            Go to Login
+          </button>
+
+          <button onClick={() => navigate("/dashboard")}>
+            Go to Dashboard
+          </button>
         </div>
-        </div>
-    );
+
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
